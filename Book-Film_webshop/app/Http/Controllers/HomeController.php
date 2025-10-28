4f193fea5d2085863a8f7ef3217ea8162597dd73
Gery0602 <107203634+Gery0602@users.noreply.github.com>
2025-10-28 14:27:12 +0100

    kezdőlap első verzió

---
+++ Book-Film_webshop/app/Http/Controllers/HomeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // Kiemelt filmek és könyvek
+        $featuredMovies = Movie::where('is_featured', true)
+            ->orderBy('created_at', 'desc')
+            ->take(6)
+            ->get();
+        
+        $featuredBooks = Book::where('is_featured', true)
+            ->orderBy('created_at', 'desc')
+            ->take(6)
+            ->get();
+        
+        // Új megjelenések
+        $newMovies = Movie::orderBy('release_year', 'desc')
+            ->take(8)
+            ->get();
+        
+        $newBooks = Book::orderBy('publication_year', 'desc')
+            ->take(8)
+            ->get();
+        
+        // Legnépszerűbb termékek
+        $popularMovies = Movie::orderBy('rating', 'desc')
+            ->take(4)
+            ->get();
+        
+        $popularBooks = Book::orderBy('rating', 'desc')
+            ->take(4)
+            ->get();
+
+        return view('home', compact(
+            'featuredMovies',
+            'featuredBooks',
+            'newMovies',
+            'newBooks',
+            'popularMovies',
+            'popularBooks'
+        ));
+    }
+}
