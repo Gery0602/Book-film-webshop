3c069878c95604638fdb29fc48b2165320abb986
Rezvadasz <botosb361@gmail.com>
2025-11-17 21:16:28 +0100

    -Film és Könyvek blade megcsinálva és bootsrapelve -Kártyák adatbázisból való létrehozása és hozzáadása a laphoz -Egy rout hozzáadva (API elkerülése érdekében)

--- mediahub/routes/web.php
+++ mediahub/routes/web.php
@@ -3,14 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\MovieController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+
+Route::get('/dashboard', [MovieController::class, 'index'])->name('dashboard');
+
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
